99078f91c538b8efd6e6df32db58bdfca31ab02f
Scott Donaldson <scottdonaldson88@gmail.com>
2021-11-08 20:41:24 +0000

    Random array gen from class

--- CA_encrypt.py
+++ CA_encrypt.py
@@ -44,6 +44,72 @@ def xorArrays(A1,A2):
     return np.array(XORed,dtype=int)
 
 
+class randEQaDG:
+    """
+    Generate a pseudo random number based on the `Even Quicker and Dirtier Generator' 
+    random number generation in [1, p275-276].
+
+    This utilises generates each random integer R using a seed S (given as initialisation param)
+    with the formula:
+
+        R = S * 1664525 + 1013904223 (mod 2^32)
+        S = R
+    
+    Where these two lines can be run repeatedly to find pseudo random integer R in [0,2^32)
+
+    IMPORTANT NOTE:
+    ==============
+    This random number generator is a quick and dirty generator, and is NOT a secure RNG.
+    It is used in this module with that knowledge.
+
+    REFERENCES:
+    ==========
+    [1] ``Numerical Recipes in Fortran 77, The Art of Scientific Computing, Vol 1'', 
+        Press W.H. and Teukolsky S.A. and Vetterling W.T. and Flannery B.P., 
+        2nd ed, Cambridge University Press.
+    """
+
+    def __init__(self,seed=3574541233091423):
+        self.rand       = seed
+        self.randBit    = None
+        self.randBitArr = None
+
+    def EQaDG(self):
+        """
+        Generate a pseudo random integer in [0,2^32) using `Even Quicker and Dirtier Generator' 
+        random number generation from [1, p275-276].
+        """
+        self.rand = ((self.rand*1664525+1013904223)%0b100000000000000000000000000000000)
+        
+    def EQaDGmp(self):
+        """
+        Generate a pseudo random integer in [-2^31,2^31) using `Even Quicker and Dirtier Generator' 
+        random number generation from [1, p275-276].
+        """        
+        self.EQaDG()
+        self.rand = self.rand - 0b10000000000000000000000000000000
+
+    def EQaDGb(self):
+        """
+        Generate a pseudo random bit in {0,1} using `Even Quicker and Dirtier Generator' 
+        random number generation from [1, p275-276].
+        """
+        self.EQaDGmp()
+        if self.rand<0:
+            self.randBit = 1
+        else:
+            self.randBit = 0
+
+    def EQaDGbA(self,length):
+        """
+        Generate an array of pseudo random bits in {0,1} using `Even Quicker and Dirtier Generator' 
+        random number generation from [1, p275-276].
+        """
+        self.randBitArr = []
+        for i in range(length):
+            self.EQaDGb()
+            self.randBitArr.append(self.randBit)
+        
 class CA:
 
     """
@@ -457,41 +523,9 @@ print("random seed:",C.randSeed)
 print("number of rules:",len(C.rules))
 print("Z_right:",C.Zright)
 
-# C.setBinStartVec([0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0])
-# print(C.start)
-# for i in range(20):
-#     C.singleCAstep()
-#     if i==19:
-#         print("\n\n")
-#     print(C.CAts)
-
-# print("-------------------------")
-# C.setBinStartVec([0,0,0,0,0,0,0,0,0,0,1])
-# C.CAsteps(20)
-# print(C.end)
-
-# print("\n\n")
-# for i in range(20):
-#     C.singleCAstepReverseL()
-#     if i==19:
-#         print("\n\n")
-#     print(C.CAts)
-
 def saveImage(fname,D1arr,outShape):
     im = Image.fromarray(np.array(np.resize(D1arr,outShape)*255,dtype=np.uint8))
     im.save(fname)
-
-def randArr(length):
-    seed = 1000
-    A = []
-    for i in range(length):
-        seed = ((1664525*seed+1013904223)%0b100000000000000000000000000000000)\
-            -0b10000000000000000000000000000000
-        if seed<0:
-            A.append(1)
-        else:
-            A.append(0)
-    return np.array(A,dtype=int)
             
 # Set initial `end point'
 C.setBinEndVec(A.flatten())
@@ -500,7 +534,13 @@ C.setBinEndVec(A.flatten())
 saveImage("input.png",C.end,Ashape)
 
 # Generate a random array (currently for testing XOR)
-randArr = randArr(len(C.end))
+R = randEQaDG()
+R.EQaDGbA(len(C.end))
+randArr = R.randBitArr
+
+print(randArr)
+
+EXIT("yo")
 
 # Xor the noise array with the end array
 C.end = xorArrays(C.end,randArr)
